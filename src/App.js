f6a3cfebe64eb8033b093e8bd57da247893505b4
tobg_ <theo.beloeil.pro@gmail.com>
2021-10-13 12:24:51 +0200

    tuto done, virtualization is a bit more clear

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './App.css';
 
 import faker from 'faker';
+import {
+  List,
+  AutoSizer,
+  CellMeasurer,
+  CellMeasurerCache
+} from 'react-virtualized';
 
 const App = () => {
+  const cache = useRef(
+    new CellMeasurerCache({
+      fixedWidth: true,
+      defaultHeight: 100,
+    })
+  );
+
   const [people, setPeople] = useState([]);
+  const [time, setTime] = useState(new Date());
 
   useEffect(() => {
     setPeople(
-      [...Array(1000).keys()].map(key => {
+      [...Array(10000).keys()].map(key => {
         return {
           id: key,
           name: faker.name.firstName(),
@@ -21,9 +35,49 @@ const App = () => {
     )
   }, []);
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className="App">
-      <ul>
+      <h1>{time.toISOString()}</h1>
+      <div style={{width: "100%", height: "90vh"}}>
+        <AutoSizer>
+          {({width, height}) => (
+            <List
+            width={width}
+            height={height}
+            rowHeight={cache.current.rowHeight}
+            deferredMeasurementCache={cache.current}
+            rowCount={people.length}
+            rowRenderer={ ({key, index, style, parent}) => {
+              const person = people[index];
+
+              return (
+                <CellMeasurer
+                  key={key}
+                  cache={cache.current}
+                  parent={parent}
+                  columnIndex={0}
+                  rowIndex={index}
+                >
+                  <div key={key} style={style}>
+                    <h2>{person.name}</h2>
+                    <p>{person.bio}</p>
+                  </div>
+                </CellMeasurer>
+              );
+            }}
+          />
+          )}
+        </AutoSizer>
+      </div>
+      {/* <ul>
         {people.map((element) => (
           <li key={element.id}>
             <h2>{element.name}</h2>
@@ -32,7 +86,7 @@ const App = () => {
               <p>address : {element.address}</p>
             </div>
           </li>))}
-      </ul>
+      </ul> */}
     </div>
   );
 }
